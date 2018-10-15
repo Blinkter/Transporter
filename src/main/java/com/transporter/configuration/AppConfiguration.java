be02fccc9c6a9bac97c72ef6bddf3f347840a1a4
Łukasz Samul <samul.luk@gmail.com>
2018-10-15 17:50:58 +0200

    Some change

--- src/main/java/com/transporter/configuration/AppConfiguration.java
+++ src/main/java/com/transporter/configuration/AppConfiguration.java
@@ -1,17 +1,29 @@
 package com.transporter.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableJpaRepositories
+@EnableTransactionManagement
 public class AppConfiguration implements WebMvcConfigurer {
 
+	@Autowired
+	private Environment environment;
+
+    /*
+     * Configure BCrypt password encoder.
+     */
 	@Bean
 	public BCryptPasswordEncoder passwordEncoder() {
 		BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
 		return bCryptPasswordEncoder;
 	}
-	
+
 }
