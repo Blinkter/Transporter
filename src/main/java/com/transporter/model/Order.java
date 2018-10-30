7695195f8a168a1c24f62908597b42e425484d04
Łukasz Samul <samul.luk@gmail.com>
2018-10-30 20:04:17 +0100

    fix order model

--- src/main/java/com/transporter/model/Order.java
+++ src/main/java/com/transporter/model/Order.java
@@ -2,6 +2,7 @@ package com.transporter.model;
 
 import java.util.Date;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -10,6 +11,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
 
 import lombok.Data;
 
@@ -20,26 +22,37 @@ public class Order {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
 	private Long id;
 	// -----------------------------------------------------------------------------
-
+	@NotEmpty
 	private String origin;
+	// -----------------------------------------------------------------------------
+	@NotEmpty
 	private String destination;
+	// -----------------------------------------------------------------------------
+	@NotEmpty
 	private String description;
+	// -----------------------------------------------------------------------------
+	@NotEmpty
 	private Date plannedDate;
+	// -----------------------------------------------------------------------------
 	private Double distance;
+	// -----------------------------------------------------------------------------
 	private Float price;
+	// -----------------------------------------------------------------------------
 	private Date acceptedDate;
-
+	// -----------------------------------------------------------------------------
 	
 	@ManyToOne 
 	@JoinColumn(name = "user_id")
 	private User user;
-
-
+	
 	@OneToOne 
 	@JoinColumn(name = "driver_id")
 	private Driver driver;
+	
+	// -----------------------------------------------------------------------------
 
 	public Order() {
 	}
@@ -55,11 +68,4 @@ public class Order {
 		this.price = price;
 		this.acceptedDate = acceptedDate;
 	}
-
-	@Override
-	public String toString() {
-		return "Transaction [id=" + id + ", origin=" + origin + ", destination=" + destination + ", distance="
-				+ distance + ", price=" + price + "]";
-	}
-
 }
